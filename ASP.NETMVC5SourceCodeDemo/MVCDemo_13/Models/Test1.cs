55d3453136112ecbce84d00ee04d458320c221e1
马鑫 <anyangmaxin@163.com>
2016-02-16 11:41:04 +0800

    把服务定位器作为一个接口，而不是一个具体类型

--- ASP.NETMVC5SourceCodeDemo/MVCDemo_13/Models/Test1.cs
+++ ASP.NETMVC5SourceCodeDemo/MVCDemo_13/Models/Test1.cs
@@ -41,4 +41,12 @@ namespace MVCDemo_13.Models
         把依赖的创建移到使用这些依赖的类的外部，这称为控制反转模式，之所以这样命名，是因为反转的是依赖的创建，正因为如此，才消除了消费者类对依赖创建的控制。
         */
     }
+
+    /// <summary>
+    /// 把服务定位器作为一个接口，而不是一个具体类型
+    /// </summary>
+    public interface IServiceLocator
+    {
+        IMessageingService GetMessagingService();
+    }
 }
