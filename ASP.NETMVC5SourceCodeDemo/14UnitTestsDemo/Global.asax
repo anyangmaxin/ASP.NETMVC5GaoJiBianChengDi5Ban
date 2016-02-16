35259b47098faafd0a285148a63498dc97cf174a
马鑫 <anyangmaxin@163.com>
2016-02-16 16:20:49 +0800

    创建单元测试项目

---
+++ ASP.NETMVC5SourceCodeDemo/14UnitTestsDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_14UnitTestsDemo.MvcApplication" Language="C#" %>
---
+++ ASP.NETMVC5SourceCodeDemo/14UnitTestsDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_14UnitTestsDemo.Startup))]
+namespace _14UnitTestsDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
