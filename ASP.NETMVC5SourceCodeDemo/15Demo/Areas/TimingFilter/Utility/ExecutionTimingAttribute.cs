6d1f5f061480b47fd663a2ac7e70bb55c0471432
马鑫 <anyangmaxin@163.com>
2016-02-18 15:56:25 +0800

    操作和结果过滤器

--- ASP.NETMVC5SourceCodeDemo/15Demo/Areas/TimingFilter/Utility/ExecutionTimingAttribute.cs
+++ ASP.NETMVC5SourceCodeDemo/15Demo/Areas/TimingFilter/Utility/ExecutionTimingAttribute.cs
@@ -9,6 +9,8 @@ namespace _15Demo.Areas.TimingFilter.Utility
     {
         IStatefulStorage storage = StatefulStorage.PerRequest;
 
+        private string actionSort = "";
+
         Stopwatch GetStopwatch(string name)
         {
             return storage.GetOrAdd<Stopwatch>(name, () => new Stopwatch());
@@ -17,34 +19,38 @@ namespace _15Demo.Areas.TimingFilter.Utility
         public override void OnActionExecuting(ActionExecutingContext filterContext)
         {
             GetStopwatch("action").Start();
+            actionSort += "[OnActionExecuting]";
         }
 
         public override void OnActionExecuted(ActionExecutedContext filterContext)
         {
             GetStopwatch("action").Stop();
+            actionSort += "[OnActionExecuted]";
         }
 
         public override void OnResultExecuting(ResultExecutingContext filterContext)
         {
             GetStopwatch("result").Start();
+            actionSort += "[OnResultExecuting]";
         }
 
         public override void OnResultExecuted(ResultExecutedContext filterContext)
         {
             var resultStopwatch = GetStopwatch("result");
             resultStopwatch.Stop();
-
+            actionSort += "[OnResultExecuted]";
             var actionStopwatch = GetStopwatch("action");
             var response = filterContext.HttpContext.Response;
 
             if (!filterContext.IsChildAction && response.ContentType == "text/html")
                 response.Write(
                     String.Format(
-                        "<h5>Action '{0} :: {1}', Execute: {2}ms, Result: {3}ms.</h5>",
+                        "<h5>Action '{0} :: {1}', Execute: {2}ms, Result: {3}ms.</h5> \r Ö´ÐÐË³Ðò£º{4}",
                         filterContext.RouteData.Values["controller"],
                         filterContext.RouteData.Values["action"],
                         actionStopwatch.ElapsedMilliseconds,
-                        resultStopwatch.ElapsedMilliseconds
+                        resultStopwatch.ElapsedMilliseconds,
+                        actionSort
                     )
                 );
         }
