c1428e31d5f587cf1e53d0100178a814cac2c4b6
马鑫 <anyangmaxin@163.com>
2016-02-18 17:42:40 +0800

    注册基于自定义准则的设备模式，例如针对WindowsPhone设备

--- ASP.NETMVC5SourceCodeDemo/16Demo/Global.asax.cs
+++ ASP.NETMVC5SourceCodeDemo/16Demo/Global.asax.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using System.Web.WebPages;
 
 namespace _16Demo
 {
@@ -11,6 +12,11 @@ namespace _16Demo
     {
         protected void Application_Start()
         {
+            //注册基于自定义准则的设备模式，例如针对WindowsPhone设备
+            DisplayModeProvider.Instance.Modes.Insert(0, new DefaultDisplayMode("WinPhone")
+            {
+                ContextCondition = (context => context.GetOverriddenUserAgent().IndexOf("Windes Phone OS", StringComparison.OrdinalIgnoreCase) >= 0)
+            });
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
