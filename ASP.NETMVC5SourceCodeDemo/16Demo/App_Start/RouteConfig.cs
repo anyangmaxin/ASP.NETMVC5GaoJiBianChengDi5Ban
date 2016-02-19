be3a056630a008c0ea05ad9d1da6511b79ab8eb1
马鑫 <anyangmaxin@163.com>
2016-02-19 16:07:47 +0800

    可编辑路由

--- ASP.NETMVC5SourceCodeDemo/16Demo/App_Start/RouteConfig.cs
+++ ASP.NETMVC5SourceCodeDemo/16Demo/App_Start/RouteConfig.cs
@@ -4,20 +4,26 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using RouteMagic;
 
 namespace _16Demo
 {
     public class RouteConfig
     {
+        //public static void RegisterRoutes(RouteCollection routes)
+        //{
+        //    routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+        //    routes.MapRoute(
+        //        name: "Default",
+        //        url: "{controller}/{action}/{id}",
+        //        defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+        //    );
+        //}
+
         public static void RegisterRoutes(RouteCollection routes)
         {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            RouteTable.Routes.RegisterRoutes("~/App_Start/Routes.cs");
         }
     }
 }
---
+++ ASP.NETMVC5SourceCodeDemo/16Demo/Models/RazorLayoutHelpers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.WebPages;
+
+namespace _16Demo.Models
+{
+    public static class RazorLayoutHelpers
+    {
+        public static HelperResult RenderSection(this WebPageBase webPage, string name, Func<dynamic, HelperResult> defaultContents)
+        {
+            if (webPage.IsSectionDefined(name))
+            {
+                return webPage.RenderSection(name);
+            }
+            return defaultContents(null);
+        }
+    }
+}
