b50c2ac70f66fdf93238ac19e47b6bd93924d87a
马鑫 <anyangmaxin@163.com>
2016-02-19 10:37:30 +0800

    视图编译，razor会在运行时动态编译成类，然后执行，编译在视图第一次被请求时发生，这会引发轻微的一次性能 开销，再次被请求时，它就会完全运行编译后的代码。

---
+++ ASP.NETMVC5SourceCodeDemo/16Demo/Models/CustomWebViewPage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.WebPages;
+
+namespace _16Demo.Models
+{
+    public abstract class CustomWebViewPage<T> : WebViewPage<T>
+    {
+        public HelperResult RenderSection(string name, Func<dynamic, HelperResult> defaultContents)
+        {
+            if (IsSectionDefined(name))
+            {
+                return RenderSection(name);
+            }
+            return defaultContents(null);
+        }
+    }
+}
